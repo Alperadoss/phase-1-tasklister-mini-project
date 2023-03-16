39d5adc0f04c73bfe3e6090c1e3bfd47d0a4eb81
Alper Yazagan <alperyazagan@gmail.com>
2023-03-16 13:52:49 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //shortenings for html elements
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("list");
+  const taskElement = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newTask = taskDescInput.innerText;
+    console.log(newTask);
+  });
 });
